ddea8c5e76372f75312b3b53f4fe4ea4cd9a2d9a
seungmin-dev <seungmin.dev@gmail.com>
2022-05-17 20:58:26 +0900

    details modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,16 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="peer rounded-md border border-gray-400 p-1"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awesome Username !
-      </span>
-      <input
-        type="submit"
-        value="Login"
-        className="rounded-lg border-2 border-gray-400 bg-white"
-      />
-    </form>
+    <div className="flex flex-col space-y-2 p-5">
+      <details className="select-none open:bg-indigo-400 open:text-white">
+        <summary className="cursor-pointer">
+          What is my favorite MCU movie
+        </summary>
+        <span className="selection:bg-indigo-400 selection:text-white">
+          Winter Soldier
+        </span>
+      </details>
+    </div>
   );
 };
 
