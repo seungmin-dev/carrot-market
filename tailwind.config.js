fefe406a58e856cd7ccbf4f4388a94d6bd68cadb
seungmin-dev <seungmin.dev@gmail.com>
2022-05-19 19:25:44 +0900

    Auth part

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  // darkMode: "media", // media로 되어있으면 환경설정, 브라우저의 설정을 따라감
-  darkMode: "class", // class로 두면 토글, 버튼 등으로 수동 설정이 가능함
-  plugins: [],
+  darkMode: "media",
+  plugins: [require("@tailwindcss/forms")],
 };
