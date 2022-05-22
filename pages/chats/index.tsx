2987483748a117f414c8f9d9d45f411a5930a30c
seungmin-dev <seungmin.dev@gmail.com>
2022-05-22 15:34:54 +0900

    Layout

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chat: NextPage = () => {
   return (
-    <div className="divide-y-[1px] py-10">
-      {[1, 1, 1, 1, 1].map((i) => (
-        <div
-          key={i}
-          className="mb-2 flex cursor-pointer items-center space-x-3 px-4 py-2"
-        >
-          <div className="h-12 w-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="font-medium text-gray-700">Steve Jebs</p>
-            <p className="text-sm font-medium text-gray-500">
-              See you tomorrow
-            </p>
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px]">
+        {[1, 1, 1, 1, 1].map((i) => (
+          <div
+            key={i}
+            className="mb-2 flex cursor-pointer items-center space-x-3 px-4 py-2"
+          >
+            <div className="h-12 w-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="font-medium text-gray-700">Steve Jebs</p>
+              <p className="text-sm font-medium text-gray-500">
+                See you tomorrow
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
