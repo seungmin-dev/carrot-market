44374596f9a6580e3f956674dbe314d7c585d6ab
seungmin-dev <seungmin.dev@gmail.com>
2022-05-25 14:25:14 +0900

    Cleaned the Form submission code

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/client";
+import client from "../../../libs/server/client";
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,5 +9,5 @@ export default async function handler(
     res.status(401).end();
   }
   console.log(req.body);
-  res.status(200).end();
+  res.json({ ok: true });
 }
