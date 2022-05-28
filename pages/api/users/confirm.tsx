641991a991480f6e3f2ca032f8fcee1ad6755fc9
seungmin-dev <seungmin.dev@gmail.com>
2022-05-28 17:48:12 +0900

    Authentication - Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,32 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import { PayloadContext } from "twilio/lib/rest/api/v2010/account/recording/addOnResult/payload";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  console.log(req.session);
+  // 아래에서 핸들러를 withIronSessionApiRoute(도우미 함수)로 감싸줬기 때문에 req.session 확인 가능
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+  if (!exists) res.status(400).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save(); // 세션 데이터를 암호화하고 쿠키를 설정
+  console.log(exists);
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: process.env.SESSION_PW!,
+});
